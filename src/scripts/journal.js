3562aaa483a3230ba13cc31740c9df6ec270f203
Bennett Foster <bennfos@gmail.com>
2019-07-11 15:20:25 -0500

    added journal objects and an entries array to hold them all

---
+++ src/scripts/journal.js
@@ -0,0 +1,35 @@
+/*
+    Define the keys and value for a JavaScript object that
+    represents a journal entry about what you learned today
+*/
+const journalEntry = {
+    date:
+    concept:
+    entry:
+    mood:
+};
+
+
+/*
+    Define the keys and value for a JavaScript object that
+    represents a journal entry about what you learned on a 
+    past day
+*/
+const pastEntry = {};
+
+/*
+    Define the keys and value for a JavaScript object that
+    represents a journal entry about what you feel you need
+    to learn more about
+*/
+const learnEntry = {};
+
+// Define an array that will hold all types of journal entries
+const entries = [];
+
+// Adds journal entry objects to entries array
+entries.push(journalEntry);
+entries.push(pastEntry, learnEntry);
+
+console.log(entries);
+
