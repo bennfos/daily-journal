fcde7b8a53aeb142e9e858f392d16c3681d5d389
Bennett Foster <bennfos@gmail.com>
2019-07-19 21:08:53 -0500

    finished some flex styling and adding all gifs

--- src/scripts/journal.js
+++ src/scripts/journal.js
@@ -16,9 +16,7 @@ const addEntry = (journalEntry) => {
 }
 
 
-
-
-
+// Declare variables that hold the strings for each mood
 const keepKicking = "I think the tide's with us. --Keep kicking."
 const catchBird = "I'll catch this bird for ya, but it ain't gonna be easy."
 const smile = "Smile, you son of a bitch."
@@ -26,12 +24,13 @@ const drowning = "There's a clinical name for it, isn't there? --Drowning."
 const farewell = "🎶Farewell and adieu to you, fair Spanish ladies🎶"
 const biggerBoat = "You're gonna need a bigger boat."
 
-const keepKickingGIF = `<img class="gifLink" src="https://media.giphy.com/media/8ABCP2H2CVNO8/giphy.gif"></img>`
-const catchBirdGIF = `<img class="gifLink" src="https://media.giphy.com/media/OfO5n2f2nuefC/giphy.gif"></img>`
-const smileGIF = `<img class="gifLink" src="https://media.giphy.com/media/OfO5n2f2nuefC/giphy.gif"></img>`
-const drowningGIF = `<img class="gifLink" src="https://media.giphy.com/media/OfO5n2f2nuefC/giphy.gif"></img>`
-const biggerBoatGIF = `<img class="gifLink" src="https://media.giphy.com/media/OfO5n2f2nuefC/giphy.gif"></img>`
-const farewellGIF = `<img class="gifLink" src="https://media.giphy.com/media/OfO5n2f2nuefC/giphy.gif"></img>`
+
+const keepKickingGIF = `<img class="gifLink" src="https://media.giphy.com/media/6uIpPo7BHmLO8/giphy.gif"></img>`
+const catchBirdGIF = `<img class="gifLink" src="https://media.giphy.com/media/8ABCP2H2CVNO8/giphy.gif"></img>`
+const smileGIF = `<img class="gifLink" src="https://media.giphy.com/media/10FxDnvl2IGvzq/giphy.gif"></img>`
+const drowningGIF = `<img class="gifLink" src="https://media.giphy.com/media/tsSp7dZRkGlK53G8vM/giphy.gif"></img>`
+const biggerBoatGIF = `<img class="gifLink" src="https://media.giphy.com/media/ds6uvzSJqgp8I/giphy.gif"></img>`
+const farewellGIF = `<img class="gifLink" src="https://media.giphy.com/media/6frspGzg5uC5i/giphy.gif"></img>`
 
 
 const makeJournalEntryComponent = (journalEntry) => {
@@ -70,18 +69,40 @@ const makeJournalEntryComponent = (journalEntry) => {
 
 //Write new entry here
 
+const test1 = {
+    date: "7/18/2019",
+    concept: "APIs, Rendering to the DOM, LinkedIn",
+    entry: "Struggling with the steps to take to get the functions to work that render HTML to the DOM. Also need to remember not to procrastinate with LinkedIn. Keep up with it and it will save a lot of time. I have very little idea what's going on with APIs, but today was just an intro",
+    mood: biggerBoat
+}
+
+const test2 = {
+    date: "7/18/2019",
+    concept: "APIs, Rendering to the DOM, LinkedIn",
+    entry: "Struggling with the steps to take to get the functions to work that render HTML to the DOM. Also need to remember not to procrastinate with LinkedIn. Keep up with it and it will save a lot of time. I have very little idea what's going on with APIs, but today was just an intro",
+    mood: farewell
+}
+
+const test3 = {
+    date: "7/18/2019",
+    concept: "APIs, Rendering to the DOM, LinkedIn",
+    entry: "Struggling with the steps to take to get the functions to work that render HTML to the DOM. Also need to remember not to procrastinate with LinkedIn. Keep up with it and it will save a lot of time. I have very little idea what's going on with APIs, but today was just an intro",
+    mood: smile
+}
+
+
 const journalEntryJuly18 = {
     date: "7/18/2019",
     concept: "APIs, Rendering to the DOM, LinkedIn",
     entry: "Struggling with the steps to take to get the functions to work that render HTML to the DOM. Also need to remember not to procrastinate with LinkedIn. Keep up with it and it will save a lot of time. I have very little idea what's going on with APIs, but today was just an intro",
-    mood: "I'll catch this bird for ya, but it ain't gonna be easy."
+    mood: catchBird
 }
 
 const journalEntryJuly17 = {
     date: "7/17/2019",
     concept: "Components and Functions",
     entry: "Learned about adding HTML components to the DOM dynamically with Javascript. Struggled with the functions but ultimately finished almost all the exercises.",
-    mood: "I think the tide's with us. --Keep kicking."
+    mood: drowning
 }
 
 const journalEntryJuly15 = {
@@ -93,6 +114,9 @@ const journalEntryJuly15 = {
 
 //Add entry to entry array here
 
+addEntry(test1)
+addEntry(test2)
+addEntry(test3)
 addEntry(journalEntryJuly18)
 addEntry(journalEntryJuly17)
 addEntry(journalEntryJuly15)
