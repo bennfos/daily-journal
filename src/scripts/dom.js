9183a3d36d8ddecd789dcc32e3baceadf73da010
Bennett Foster <bennfos@gmail.com>
2019-08-12 13:50:56 -0500

    sorted entries by date, added some button styling

--- src/scripts/dom.js
+++ src/scripts/dom.js
@@ -3,9 +3,22 @@
 
 import factoryObject from "./factory.js"
 
+const sortEntries = (entries) => {
+    entries.sort(function(a, b) {
+        if (a.date < b.date) {
+            return 1
+        }
+        if (a.date > b.date) {
+            return -1
+        }
+        return 0;
+    })
+}
+
 const render = {
     renderEntry (entries) {
         factoryObject.entryContainer.innerHTML = ""
+        sortEntries(entries)
         for (let entry of entries) {
             let entryHTML = factoryObject.factory.makeJournalEntryComponent(entry)
         factoryObject.entryContainer.innerHTML += entryHTML
